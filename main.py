ebc1d25acec254675e9966ee892d5ad9e81501d6
dpFHGR <163541212+dpFHGR@users.noreply.github.com>
2025-04-04 09:56:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from dotenv import dotenv_values
-from db import get_database # Import from db.py
-from routes import router as monitoring_router
+from pymongo import MongoClient
+from routes import template_router, tool_router
 
 config = dotenv_values(".env")
 
 app = FastAPI()
 
-app.include_router(monitoring_router, tags=["monitoring"], prefix="/monitoring")
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Monitoring Template Repository!"}
+
+app.include_router(template_router, tags=["monitoring_templates"], prefix="/template")
+app.include_router(tool_router, tags=["monitoring_tools"], prefix="/tools")
